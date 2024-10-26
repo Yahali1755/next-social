cdf38da4abaf5594bc5955d273dfb7e2423f85dc
erwre <yahali100@gmail.com>
2024-10-26 15:13:01 +0300

    kuy

---
+++ src/components/rightMenu/RecomendedUsers.tsx
@@ -0,0 +1,38 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import FollowRequestList from "./FollowRequestList";
+
+const RecomendedUsers = async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const requests = await prisma.followRequest.findMany({
+    where: {
+      NOT: {
+        receiverId: userId,
+      }
+    },
+    include: {
+      sender: true,
+    },
+  });
+
+  if (requests.length === 0) return null;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* TOP */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">Follow Requests</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+      {/* USER */}
+      <FollowRequestList requests={requests}/>
+    </div>
+  );
+};
+
+export default RecomendedUsers;
